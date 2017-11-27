1a224c05854c5f44955b717058656a78ff8de985
Konstantinos Paliouras <squarious@gmail.com>
2017-11-27 11:06:28 +0200

    [examples] Update planar dataset with spyral and flower

--- examples/planar.py
+++ examples/planar.py
@@ -26,6 +26,45 @@ def load_planar_dataset():
     return X, Y
 
 
+def load_flower_dataset():
+    np.random.seed(1)
+    m = 400  # number of examples
+    N = int(m / 2)  # number of points per class
+    D = 2  # dimensionality
+    X = np.zeros((m, D))  # data matrix where each row is a single example
+    Y = np.zeros((m, 1), dtype='uint8')  # labels vector (0 for red, 1 for blue)
+    a = 4  # maximum ray of the flower
+
+    for j in range(2):
+        ix = range(N * j, N * (j + 1))
+        t = np.linspace(j * 3.12, (j + 1) * 3.12, N) + np.random.randn(N) * 0.2  # theta
+        r = a * np.sin(4 * t) + np.random.randn(N) * 0.2  # radius
+        X[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
+        Y[ix] = j
+
+    X = X.T
+    Y = Y.T
+
+    return X, Y
+
+
+def load_spyral_dataset(N=100):
+    # Taken from Standford's cs231n https://cs231n.github.io/neural-networks-case-study/
+
+    # N = 100  # number of points per class
+    D = 2  # dimensionality
+    K = 3  # number of classes
+    X = np.zeros((N * K, D))  # data matrix (each row = single example)
+    y = np.zeros(N * K, dtype='uint8')  # class labels
+    for j in range(K):
+        ix = range(N * j, N * (j + 1))
+        r = np.linspace(0.0, 1, N)  # radius
+        t = np.linspace(j * 4, (j + 1) * 4, N) + np.random.randn(N) * 0.2  # theta
+        X[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
+        y[ix] = j
+    return X, y
+
+
 def plot_decision_boundary(model, X, y):
     # Set min and max values and give it some padding
     x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
