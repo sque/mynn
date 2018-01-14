56683903a107eb8befb35831f8b1191771f5f29b
Konstantinos Paliouras <squarious@gmail.com>
2018-01-14 14:49:59 +0200

    [initializers] Update initializers for the new type of network

--- mynn/initializers.py
+++ mynn/initializers.py
@@ -1,19 +1,22 @@
-from typing import Union, List, Tuple
+from abc import ABCMeta, abstractmethod
+from typing import Union, Dict, Tuple
+from .layers import Layer
+import logging as _logging
 import numpy as np
 
+logger = _logging.getLogger(__name__)
 
-class WeightInitializerBase:
-    pass
 
-    def get_initial_weight(self, l: int, n: int, n_left: int) -> Tuple[np.ndarray, np.ndarray]:
+class WeightInitializerBase(metaclass=ABCMeta):
+    """Base class for implementing initializer of layer's trainable parameters"""
+
+    @abstractmethod
+    def __call__(self, layer: Layer):
         """
-        Get the initial weights of the linear functions Z
-        :param l: The level of the layer 1-indexed
-        :param n: The number of nodes in the layer (Noutput)
-        :param n_left: The number of nodes in the previous layer (Ninput)
-        :return: An array of shape (n, n_left) with the initial values of linear weights as also the b weights
+        Initialize trainable parameters of a layer, usually called weights
+        :param layer: The layer to initialize weights
         """
-        raise NotImplementedError()
+        pass
 
 
 class ConstantWeightInitializer(WeightInitializerBase):
@@ -21,26 +24,31 @@ class ConstantWeightInitializer(WeightInitializerBase):
     It initializes all weights with constant values.
     """
 
-    def __init__(self, weights: List[Union[np.array, float]]):
+    def __init__(self, weights: Dict[str, Dict[str, Union[np.array, float]]]):
         """
         Create a constant weight initializer
-        :param weights: A List with constant weights per layer. For each layer you can give
-        a correct shaped array or a float to be broadcast.
+        :param weights: Weights mapped per layer and parameter name. The weights can
+        be given as a full array or as a float that will be broadcasted to the shape
+        of the array.
         """
         self._weights = weights
 
-    def get_initial_weight(self, l: int, n: int, n_left: int) -> Tuple[np.ndarray, np.ndarray]:
-        layer_weights = self._weights[l - 1]
-        b = np.zeros((n, 1))
+    def __call__(self, layer: Layer):
 
-        if isinstance(layer_weights, (float, int)):
-            return np.ones((n, n_left)) * layer_weights, b
-        elif isinstance(layer_weights, np.ndarray):
-            if layer_weights.shape != (n, n_left):
-                raise ValueError(f"The provided weight array is not of the correct shape for layer {l}")
-            return layer_weights, b
+        if not layer.name in self._weights:
+            logger.warning(f"Couldn't find constant weights for layer {layer.name}. Skipping")
+            return
 
-        raise TypeError(f"Unexpected type of: {type(layer_weights)} for layer {l}")
+        layer_weights = self._weights[layer.name]
+        for pname, info in layer.parameters_info.items():
+            if pname not in layer_weights:
+                logger.warning(f"Couldn't find constant weights for parameter {pname} of "
+                               f"layer {layer.name}. Skipping")
+                continue
+            param_weights = layer_weights[pname]
+            if not isinstance(param_weights, np.ndarray):
+                param_weights = np.ones(info.shape) * param_weights
+            layer.parameters = {pname: param_weights}
 
 
 class NormalWeightInitializer(WeightInitializerBase):
@@ -48,29 +56,46 @@ class NormalWeightInitializer(WeightInitializerBase):
     Weight initializer with normal distribution
     """
 
-    def get_initial_weight(self, l: int, n: int, n_left: int) -> Tuple[np.ndarray, np.ndarray]:
-        W = np.random.randn(n, n_left)
-        b = np.zeros((n, 1))
-        return W, b
+    def __init__(self, scale: float):
+        """
+        :param scale: The maximum values of weights.
+        """
+        self._scale = scale
+
+    def __call__(self, layer: Layer):
+
+        for p, info in layer.parameters_info.items():
+            if info.init_random:
+                layer.parameters = {p: np.random.rand(*info.shape) * self._scale}
+            else:
+                layer.parameters = {p: np.zeros(info.shape)}
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}()"
 
 
-class VarianceScalingWeightInitializer(WeightInitializerBase):
+class XavierWeightInitializer(WeightInitializerBase):
     """
     A general form of an initializer that tries to scale the variance of the weights
-
     """
 
     def __init__(self, scale: float = 2):
         """
-        Weights will be initialized as W = randn() * np.sqrt(scale / n_left)
+        Weights will be initialized as W = randn() * np.sqrt(scale / n_in + n_out)
         :param scale: The scale factor of the final variance
         """
         self._scale = scale
 
-    def get_initial_weight(self, l: int, n: int, n_left: int) -> Tuple[np.ndarray, np.ndarray]:
-        W = np.random.randn(n, n_left) * np.sqrt(self._scale / n_left)
-        b = np.zeros((n, 1))
-        return W, b
+    def __call__(self, layer: Layer):
+
+        for p, info in layer.parameters_info.items():
+            if info.init_random:
+                layer.parameters = {
+                    p: np.random.randn(*info.shape) * np.sqrt(
+                        self._scale / (layer.input_shape[0] + layer.output_shape[0]))
+                }
+            else:
+                layer.parameters = {p: np.zeros(info.shape)}
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self._scale})"
--- tests/initializers_tests.py
+++ tests/initializers_tests.py
@@ -1,104 +1,148 @@
 import numpy as np
 import unittest
 
-from mynn.initializers import ConstantWeightInitializer, NormalWeightInitializer, VarianceScalingWeightInitializer
+from mynn.initializers import ConstantWeightInitializer, NormalWeightInitializer, XavierWeightInitializer
+from mynn.layers import FullyConnected, Input
 
 
 class ConstantWeightInitializerTestCase(unittest.TestCase):
-    def test_out_of_index(self):
-        i = ConstantWeightInitializer([1, 2, 3])
-        with self.assertRaises(IndexError):
-            i.get_initial_weight(4, 4, 5)
+
+    def setUp(self):
+        i = Input((2, None))
+        self.fc = FullyConnected(3, name='fc')(i)
+        self.fc2 = FullyConnected(4, name='fc2')(self.fc)
 
     def test_broadcasting(self):
-        i = ConstantWeightInitializer([1, 2, 3])
+        i = ConstantWeightInitializer({
+            'fc': {'W': 0.5, 'b': 15}})
 
-        W, b = i.get_initial_weight(1, 4, 5)
+        i(self.fc)
 
         self.assertTrue(np.all(
-            W == np.ones((4, 5))
+            self.fc.parameters['b'] == np.ones((3, 1)) * 15
         ))
 
         self.assertTrue(np.all(
-            b == np.zeros((4, 1))
+            self.fc.parameters['W'] == np.ones((3, 2)) * 0.5
         ))
 
-    def test_array_assignment(self):
-        W1 = np.array([[1, 2, 3], [3, 4, 5]])
-        W2 = np.array([[7, 5, 4], [0., 2, 1.], [5, 2, 5]])
-        i = ConstantWeightInitializer([W1, W2])
+    def test_exact(self):
+        W1 = np.array([[1, 2], [2, 3], [4, 5]])
+        W2 = np.array([[11, 12, 13], [14, 15, 16], [17, 18, 19], [20, 21, 22]])
 
-        W, b = i.get_initial_weight(1, 2, 3)
-        self.assertTrue(np.all(W == W1))
-        self.assertTrue(np.all(b == np.zeros((2, 1))))
+        i = ConstantWeightInitializer({
+            'fc': {'W': W1, 'b': 15},
+            'fc2': {'W': W2, 'b': 0.5}})
 
-        W, b = i.get_initial_weight(2, 3, 3)
-        self.assertTrue(np.all(W == W2))
-        self.assertTrue(np.all(b == np.zeros((3, 1))))
+        i(self.fc)
+        i(self.fc2)
+
+        self.assertTrue(np.all(
+            self.fc.parameters['b'] == np.ones((3, 1)) * 15
+        ))
+
+        self.assertTrue(np.all(
+            self.fc.parameters['W'] == W1
+        ))
+
+        self.assertTrue(np.all(
+            self.fc2.parameters['b'] == np.ones((4, 1)) * 0.5
+        ))
+
+        self.assertTrue(np.all(
+            self.fc2.parameters['W'] == W2
+        ))
 
     def test_array_wrong_shape(self):
-        W1 = np.array([[1, 2, 3], [3, 4, 5]])
-        W2 = np.array([[7, 5, 4], [0., 2, 1.], [5, 2, 5]])
-        i = ConstantWeightInitializer([W1, W2])
+        W = np.array([[1, 2], [2, 3], [4, 5]])
 
-        with self.assertRaises(ValueError):
-            i.get_initial_weight(1, 10, 3)
+        i = ConstantWeightInitializer({
 
-        with self.assertRaises(ValueError):
-            i.get_initial_weight(1, 2, 10)
+            'fc2': {'W': W, 'b': 0.5}})
+
+        with self.assertRaises(TypeError):
+            i(self.fc2)
 
 
 class NormalWeightInitializerTestCase(unittest.TestCase):
-    def test_get_initial_weights(self):
+
+    def setUp(self):
+        i = Input((2, None))
+        self.fc = FullyConnected(3, name='fc')(i)
+        self.fc2 = FullyConnected(4, name='fc2')(self.fc)
+
+    def test_initialize(self):
         np.random.seed(1)
-        i = NormalWeightInitializer()
+        i = NormalWeightInitializer(0.5)
 
-        W, b = i.get_initial_weight(1, 4, 5)
-        self.assertEqual(W.shape, (4, 5))
-        random_normal = np.array([[1.62434536, -0.61175641, -0.52817175, -1.07296862, 0.86540763],
-                                  [-2.3015387, 1.74481176, -0.7612069, 0.3190391, -0.24937038],
-                                  [1.46210794, -2.06014071, -0.3224172, -0.38405435, 1.13376944],
-                                  [-1.09989127, -0.17242821, -0.87785842, 0.04221375, 0.58281521]])
+        i(self.fc)
 
-        self.assertTrue(np.allclose(W, random_normal))
+        # Check not random equal to zero
+        self.assertTrue(np.all(np.zeros((3, 1)) == self.fc.parameters['b']))
 
-        self.assertTrue(np.all(
-            b == np.zeros((4, 1))
-        ))
+        expected_W = np.array([[2.08511002e-01, 3.60162247e-01],
+                               [5.71874087e-05, 1.51166286e-01],
+                               [7.33779454e-02, 4.61692974e-02]])
+
+        self.assertTrue(np.allclose(
+            expected_W,
+            self.fc.parameters['W']))
+
+    def test_initialize_with_scale(self):
+        np.random.seed(1)
+        i = NormalWeightInitializer(100)
+
+        i(self.fc)
+
+        # Check not random equal to zero
+        self.assertTrue(np.all(np.zeros((3, 1)) == self.fc.parameters['b']))
+
+        expected_W = np.array([[4.17022005e+01, 7.20324493e+01],
+                               [1.14374817e-02, 3.02332573e+01],
+                               [1.46755891e+01, 9.23385948e+00]])
+
+        self.assertTrue(np.allclose(
+            expected_W,
+            self.fc.parameters['W']))
 
 
-class VarianceScalingWeightInitializerTestCase(unittest.TestCase):
+
+
+class XavierWeightInitializerTestCase(unittest.TestCase):
+
     def setUp(self):
         self.normal_random = np.array([[1.62434536, -0.61175641, -0.52817175, -1.07296862, 0.86540763],
                                        [-2.3015387, 1.74481176, -0.7612069, 0.3190391, -0.24937038],
                                        [1.46210794, -2.06014071, -0.3224172, -0.38405435, 1.13376944],
                                        [-1.09989127, -0.17242821, -0.87785842, 0.04221375, 0.58281521]])
 
+        i = Input((2, None))
+        self.fc = FullyConnected(3, name='fc')(i)
+        self.fc2 = FullyConnected(4, name='fc2')(self.fc)
+
     def test_get_initial_weights_with_scale2(self):
         np.random.seed(1)
-        i = VarianceScalingWeightInitializer(scale=2)
+        i = XavierWeightInitializer(scale=2)
 
-        W, b = i.get_initial_weight(1, 4, 5)
-        self.assertEqual(W.shape, (4, 5))
+        i(self.fc)
 
-        self.assertTrue(np.allclose(W, self.normal_random * np.sqrt(2 / 5)))
+        # Check not random equal to zero
+        self.assertTrue(np.all(np.zeros((3, 1)) == self.fc.parameters['b']))
 
-        self.assertTrue(np.all(
-            b == np.zeros((4, 1))
-        ))
+        expected_W = (self.normal_random.ravel() * np.sqrt(2 / 5))[:6].reshape(3, 2)
+        self.assertTrue(np.allclose(self.fc.parameters['W'], expected_W))
 
     def test_get_initial_weights_with_scale3(self):
         np.random.seed(1)
-        i = VarianceScalingWeightInitializer(scale=3)
+        i = XavierWeightInitializer(scale=3)
 
-        W, b = i.get_initial_weight(1, 4, 5)
-        self.assertEqual(W.shape, (4, 5))
+        i(self.fc)
 
-        self.assertTrue(np.allclose(W, self.normal_random * np.sqrt(3 / 5)))
+        # Check not random equal to zero
+        self.assertTrue(np.all(np.zeros((3, 1)) == self.fc.parameters['b']))
 
-        self.assertTrue(np.all(
-            b == np.zeros((4, 1))
-        ))
+        expected_W = (self.normal_random.ravel() * np.sqrt(3 / 5))[:6].reshape(3, 2)
+        self.assertTrue(np.allclose(self.fc.parameters['W'], expected_W))
 
 
 if __name__ == '__main__':
